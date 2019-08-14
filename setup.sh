1a9fecc8e1fb69eb147606da364e4553a37dd8ae
Eduards Sizovs <eduards@sizovs.net>
2019-08-14 19:41:16 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,3 +11,237 @@ echo "I  ❤️  🍎"
 echo "Mac OS Install Setup Script"
 echo "By Eduards Sizovs"
 echo "Follow me on twitter! https://twitter.com/eduardsi"
+
+##############################
+# Prerequisite: Install Brew #
+##############################
+
+echo "Installing brew..."
+
+if test ! $(which brew)
+then
+	## Don't prompt for confirmation when installing homebrew
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null
+fi
+
+# Latest brew, install brew cask
+brew upgrade
+brew update
+brew tap caskroom/cask
+
+#############################################
+### Generate ssh keys & add to ssh-agent
+### See: https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/
+#############################################
+
+echo "Generating ssh keys, adding to ssh-agent..."
+read -p 'Input email for ssh key: ' useremail
+
+echo "Use default ssh file location, enter a passphrase: "
+ssh-keygen -t rsa -b 4096 -C "$useremail"  # will prompt for password
+eval "$(ssh-agent -s)"
+
+# Now that sshconfig is synced add key to ssh-agent and
+# store passphrase in keychain
+ssh-add -K ~/.ssh/id_rsa
+
+# If you're using macOS Sierra 10.12.2 or later, you will need to modify your ~/.ssh/config file to automatically load keys into the ssh-agent and store passphrases in your keychain.
+
+if [ -e ~/.ssh/config ]
+then
+    echo "ssh config already exists. Skipping adding osx specific settings... "
+else
+	echo "Writing osx specific settings to ssh config... "
+   cat <<EOT >> ~/.ssh/config
+	Host *
+		AddKeysToAgent yes
+		UseKeychain yes
+		IdentityFile ~/.ssh/id_rsa
+EOT
+fi
+
+##############################
+# Install via Brew           #
+##############################
+
+echo "Starting brew installs..."
+
+brew install git
+brew install wget
+brew install autojump
+brew install exa
+brew install bat
+brew install fd
+brew install fzf
+brew install m-cli
+brew install the_silver_searcher
+brew install tldr
+brew install transcrypt
+brew install fx
+brew install yq
+brew install httpie
+brew cask install iterm2
+brew cask install docker
+brew cask install todoist
+brew cask install adoptopenjdk
+brew cask install vlc
+brew cask install google-chrome
+brew cask install firefox
+brew cask install 1password
+brew cask install alfred
+brew cask install imageoptim
+brew cask install intellij-idea-ce
+brew cask install karabiner-elements
+brew cask install paste
+brew cask install slack
+brew cask install bear
+brew cask install spectacle
+brew cask install telegram
+brew cask install visual-studio-code
+brew cask install caffeine  # keep computer from sleeping
+
+#############################################
+### Fonts
+#############################################
+
+echo "Installing fonts..."
+brew tap caskroom/fonts
+brew cask install font-fira-code
+
+
+#############################################
+### Installs from Mac App Store
+#############################################
+
+echo "Installing apps from the App Store..."
+brew install mas
+
+echo "Need to log in to App Store manually to install apps with mas...." 
+echo "Opening App Store. Please login."
+open "/Applications/App Store.app"
+echo "Is app store login complete.(y/n)? "
+read response
+if [ "$response" != "${response#[Yy]}" ]
+then
+	mas install 507257563  # Sip
+	mas install 928871589  # Noizio
+	mas install 1091189122 # Bear
+	mas install 585829637  # Todoist
+else
+	echo "App Store login not complete. Skipping installing App Store Apps" 
+fi
+
+
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
+# Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons
+defaults write com.apple.finder QuitMenuItem -bool true
+
+# Keep folders on top when sorting by name
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+# Only Show Open Applications In The Dock  
+defaults write com.apple.dock static-only -bool true
+
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Automatically quit printer app once the print jobs complete
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+# Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Use list view in all Finder windows by default
+# Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+
+# Minimize windows into their application’s icon
+defaults write com.apple.dock minimize-to-application -bool true
+
+
+# Don’t show recent applications in Dock
+#    defaults write com.apple.dock show-recents -bool false
+
+# Menu bar: hide the Time Machine, User icons, but show the volume Icon.
+for domain in ~/Library/Preferences/ByHost/com.apple.systemuiserver.*; do
+	defaults write "${domain}" dontAutoLoad -array \
+		"/System/Library/CoreServices/Menu Extras/TimeMachine.menu" \
+		"/System/Library/CoreServices/Menu Extras/User.menu"
+done
+defaults write com.apple.systemuiserver menuExtras -array \
+	"/System/Library/CoreServices/Menu Extras/Volume.menu" \
+	"/System/Library/CoreServices/Menu Extras/Bluetooth.menu" \
+	"/System/Library/CoreServices/Menu Extras/AirPort.menu" \
+	"/System/Library/CoreServices/Menu Extras/Battery.menu" \
+	"/System/Library/CoreServices/Menu Extras/Clock.menu"
+  
+  # Disable smart quotes and smart dashes
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+# Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+# Use function F1, F, etc keys as standard function keys
+defaults write NSGlobalDomain com.apple.keyboard.fnState -bool true
+
+# Save screenshots to the desktop
+defaults write com.apple.screencapture location -string "$HOME/Desktop"
+
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+defaults write com.apple.screencapture type -string "png"
+
+# Disable shadow in screenshots
+defaults write com.apple.screencapture disable-shadow -bool true
+
+# Trackpad: enable tap to click for this user and for the login screen
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+###############################################################################
+# Mac App Store                                                               #
+###############################################################################
+
+# Enable the automatic update check
+defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
+
+# Download newly available updates in background
+defaults write com.apple.SoftwareUpdate AutomaticDownload -int 1
+
+# Install System data files & security updates
+defaults write com.apple.SoftwareUpdate CriticalUpdateInstall -int 1
+
+# Prevent Photos from opening automatically when devices are plugged in
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
+
+echo ""
+cech "Done!"
+echo ""
+echo ""
+echo "################################################################################"
+echo ""
+echo ""
+echo "Note that some of these changes require a logout/restart to take effect."
+echo ""
+echo ""
+echo -n "Check for and install available OSX updates, install, and automatically restart? (y/n)? "
+read response
+if [ "$response" != "${response#[Yy]}" ] ;then
+    softwareupdate -i -a --restart
+fi
