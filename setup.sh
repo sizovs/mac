16b7ec9fdf915f0a2e2d2f143ac379e2f18cd4ce
Eduards Sizovs <eduards@sizovs.net>
2019-08-14 19:23:29 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#                    _           _        _ _ 
+#  ___  _____  __   (_)_ __  ___| |_ __ _| | |
+# / _ \/ __\ \/ /   | | '_ \/ __| __/ _` | | |
+#| (_) \__ \>  <    | | | | \__ \ || (_| | | |
+# \___/|___/_/\_\   |_|_| |_|___/\__\__,_|_|_|
+
+
+echo "I  ❤️  🍎"
+echo "Mac OS Install Setup Script"
+echo "By Eduards Sizovs"
+echo "Follow me on twitter! https://twitter.com/eduardsi"
